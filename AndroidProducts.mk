04fc7fb7f6ea3f4cc26832104576b08c032a0edf
Cyderxxv <phamnguyengiakhiem@gmail.com>
2023-12-14 16:59:14 +0700

    crownlte: Initial EvolutionX 8.x bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_crownlte.mk
+    $(LOCAL_DIR)/evolution_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_crownlte-eng \
-    derp_crownlte-user \
-    derp_crownlte-userdebug
+    evolution_crownlte-eng \
+    evolution_crownlte-user \
+    evolution_crownlte-userdebug
